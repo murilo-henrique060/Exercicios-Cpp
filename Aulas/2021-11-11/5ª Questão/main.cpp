1f2884755847df41363ebf0de6d5891b8af8edbe
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2021-11-18 08:18:53 -0300

    Update main.cpp

--- Aulas/2021-11-11/5ª Questão/main.cpp
+++ Aulas/2021-11-11/5ª Questão/main.cpp
@@ -5,18 +5,13 @@ int main(){
 	int vet[10], i, j, menor;
 	
 	for (i = 0; i < 10; i++){
-		cout << "Digite o " << i + 1 << "§ N£mero: ";
+		cout << "Digite o " << i + 1 << "Â§ NÂ£mero: ";
 		cin >> vet[i];
 	}
 	
 	for (i = 9; i > 0; i--){
 		for (j = 0; j < i ; j++){
-			cout << j;
-
-			if (i == 0){
-				vet[i] = vet[j];
-			}
-			else if (vet[j] < vet[i]){
+			if (vet[j] < vet[i]){
 				menor = vet[j];
 				vet[j] = vet[i];
 				vet[i] = menor;
